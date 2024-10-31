18f4de054b94613c83e24a56492d0beee136bb5c
abdosql <abdosql@users.noreply.github.com>
2024-10-31 22:23:15 +0100

    init docker env

---
+++ backend/src/config/settings.py
@@ -0,0 +1,5 @@
+# Update ALLOWED_HOSTS in settings.py
+ALLOWED_HOSTS = ['*']  # For development only. Configure properly for production
+
+# Add CORS settings if you're going to connect with frontend
+CORS_ALLOW_ALL_ORIGINS = True  # For development only 
